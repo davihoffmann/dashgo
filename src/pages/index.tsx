d08b82f4255cdeac0e274d2cd5e635c6e5935039
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2022-01-20 13:58:38 -0300

    Desenvolvimento da pagina de signIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,45 @@
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+
 export default function Home() {
   return (
-    <h1>DashGo</h1>
+    <Flex w="100vh" h="100vh" alignItems="center" justifyContent="center">
+      <Flex as="form" width="100%" maxW={360} bg="gray.800" p={8} borderRadius={8} flexDir="column">
+        <Stack spacing={4}>
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input 
+              id="email"
+              name="email" 
+              type="email" 
+              focusBorderColor="pink.500" 
+              bg="gray.900" 
+              variant="filled"
+              _hover={{ 
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+        
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input 
+              id="password"
+              name="password" 
+              type="password" 
+              focusBorderColor="pink.500" 
+              bg="gray.900" 
+              variant="filled"
+              _hover={{ 
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" colorScheme="pink" marginTop="6" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
