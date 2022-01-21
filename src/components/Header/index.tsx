ffb2e8806de0b1491605336d907fb628148e1128
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2022-01-21 16:17:10 -0300

    Desenvolvimento da Siderbar responsiva utilizando componente Drawer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../context/SidebarDrawerContext';
+
 import Logo from './Logo';
 import NotificationsNav from './NotificationsNav';
 import Profile from './Profile';
 import SearchBox from './SearchBox';
 
 export default function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -12,6 +17,18 @@ export default function Header() {
 
   return (
     <Flex as="header" w="100%" h="20" maxWidth={1480} mx="auto" mt="4" px="6" alignItems="center">
+      {
+        !isWideVersion && (
+          <IconButton
+            aria-label="Open navigation"
+            icon={<Icon as={RiMenuLine} />}
+            fontSize="24"
+            variant="unstyled"
+            onClick={onOpen}
+            mr="2"
+          />
+        )
+      }
       <Logo />
       {
         isWideVersion && (
