23637e556ef20efc3b77e24aed8146d4601046f1
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2022-01-21 13:28:44 -0300

    ajustes na tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Input from '../components/Form/Input';
 
 export default function SignIn() {
   return (
-    <Flex w="100vh" h="100vh" alignItems="center" justifyContent="center">
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
       <Flex as="form" width="100%" maxW={360} bg="gray.800" p={8} borderRadius={8} flexDir="column">
         <Stack spacing={4}>
           <Input
