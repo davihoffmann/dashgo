b9a62210c81bcc123500a55360aeb81b58372b1c
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2022-01-25 08:10:57 -0300

    Desenvolvimento inicial da logica de paginacao

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,18 +1,34 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
-import Link from "next/link";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+  useBreakpointValue,
+  Spinner,
+} from '@chakra-ui/react';
+import Link from 'next/link';
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
-import Header from "../../components/Header";
-import Pagination from "../../components/Pagination";
-import Sidebar from "../../components/Sidebar";
-import { useUsers } from "../../services/hooks/useUsers";
+import Header from '../../components/Header';
+import Pagination from '../../components/Pagination';
+import Sidebar from '../../components/Sidebar';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
-    lg: true
+    lg: true,
   });
 
   return (
@@ -26,88 +42,81 @@ export default function UserList() {
           <Flex mb="8" justifyContent="space-between" alignItems="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
-              {
-                !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />
-              }
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
-              <Button 
-                as="a" 
-                size="sm" 
-                fontSize="sm" 
-                colorScheme="pink" 
-                leftIcon={<Icon as={RiAddLine} 
-                fontSize="20" />}
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
               >
                 Criar novo
               </Button>
             </Link>
           </Flex>
 
-          {
-            isLoading ? (
-              <Flex justifyContent="center">
-                <Spinner />
-              </Flex>
-            ) : error ? (
-              <Flex justifyContent="center">
-                <Text>Falha ao obter dados dos usuários</Text>
-              </Flex>
-            ) : (
-              <>
-                <Table colorScheme="whiteAlpha">
-                  <Thead>
-                    <Tr>
-                      <Th px={["4", "4", "6"]} color="gray.300" width="8">
+          {isLoading ? (
+            <Flex justifyContent="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justifyContent="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de Cadastro</Th>}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map((user) => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
                         <Checkbox colorScheme="pink" />
-                      </Th>
-                      <Th>Usuário</Th>
-                      {isWideVersion && <Th>Data de Cadastro</Th>}
-                      <Th width="8"></Th>
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="small" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="20" />}
+                        >
+                          {isWideVersion ? 'Editar' : ''}
+                        </Button>
+                      </Td>
                     </Tr>
-                  </Thead>
-                  <Tbody>
-                    {
-                      data.map(user => (
-                        <Tr key={user.id}>
-                          <Td px={["4", "4", "6"]}>
-                            <Checkbox colorScheme="pink" />
-                          </Td>
-                          <Td>
-                            <Box>
-                              <Text fontWeight="bold">{user.name}</Text>
-                              <Text fontSize="small" color="gray.300">{user.email}</Text>
-                            </Box>
-                          </Td>
-                          {isWideVersion && (
-                            <Td>
-                              {user.createdAt}
-                            </Td>
-                          )}
-                          <Td>
-                            <Button 
-                              as="a" size="sm" 
-                              fontSize="sm" 
-                              colorScheme="purple" 
-                              leftIcon={<Icon as={RiPencilLine} 
-                              fontSize="20" />}>
-                                {isWideVersion ? 'Editar' : ''}
-                            </Button>
-                          </Td>
-                        </Tr>
-                      ))
-                    }
+                  ))}
+                </Tbody>
+              </Table>
 
-                  </Tbody>
-                </Table>
-
-                <Pagination />
-              </>
-            )
-          }
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
-  )
+  );
 }
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 import faker from 'faker';
 
 type User = {
@@ -10,37 +10,51 @@ type User = {
 export function makeServer() {
   const server = createServer({
     models: {
-      user: Model.extend<Partial<User>>({})
+      user: Model.extend<Partial<User>>({}),
     },
 
     factories: {
       user: Factory.extend({
         name(i) {
-          return `User ${i+1}`
+          return `User ${i + 1}`;
         },
         email() {
           return faker.internet.email().toLocaleLowerCase();
         },
         createdAt() {
           return faker.date.recent(10);
-        }
-      })
+        },
+      }),
     },
 
     seeds(server) {
-      server.createList('user', 10);
+      server.createList('user', 200);
     },
 
     routes() {
       this.namespace = 'api';
       this.timing = 750;
 
-      this.get('/users');
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all('user').length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(200, { 'x-total-count': String(total) }, { users });
+      });
       this.post('/users');
 
       this.namespace = '';
       this.passthrough();
-    }
+    },
   });
 
   return server;
