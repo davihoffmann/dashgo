af55c8d7eec97425640412a7cc20367eca30231c
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2022-01-25 14:39:20 -0300

    finalizado desenvolvimento da paginação dos dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -26,12 +26,14 @@ import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
   const [page, setPage] = useState(1);
-  const { data, isLoading, isFetching, error } = useUsers();
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  console.log(page)
 
   return (
     <Box>
@@ -84,7 +86,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => (
+                  {data.users.map((user) => (
                     <Tr key={user.id}>
                       <Td px={['4', '4', '6']}>
                         <Checkbox colorScheme="pink" />
@@ -115,8 +117,8 @@ export default function UserList() {
               </Table>
 
               <Pagination
-                totalCountOfRegisters={200}
-                currentPage={5}
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
                 onPageChange={setPage}
               />
             </>
